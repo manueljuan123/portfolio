186779870813c7e6db58208c6c0ac8cec5b7edcb
manueljuan123 <79883680+manueljuan123@users.noreply.github.com>
2025-04-05 21:21:35 -0500

    Deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://juanmanuel1906.github.io",
-  base: "/portfolio",
+  site: 'https://porfolio/'
 });
